e61a2ae0bbb3a48309b40ba162c1d1827bd38b66
Gn0lee <jinho605@gmail.com>
2021-11-13 13:14:56 +0900

    2110 승현도움으로 풀이

--- 이진호/2110.py
+++ 이진호/2110.py
@@ -1,40 +1,32 @@
-from statistics import median_low, median_high
 import sys
-import heapq
-from collections import deque
+
 num_house , num_wifi = list(map(int,sys.stdin.readline().split()))
 house_list = [int(sys.stdin.readline()) for _ in range(num_house)]
 house_list.sort()
 wifi_index_list = []
-global cnt
-cnt = 0
+global ans
+ans = 0
 
 
 
 def wifi_search(arr,start,end,n):
-    global cnt
-
-    if n-2 == cnt:
-        return
-    mid  = median_low(arr)
-    mid_index = arr.index(mid)
-    wifi_index_list.append(mid)
-    cnt += 1
-    wifi_search(arr,start,mid_index,n)
-    wifi_search(arr,mid_index,end,n)
-
-wifi_index_list.append(house_list[0])
-wifi_search(house_list,0,num_house,num_wifi)
-wifi_index_list.append(house_list[-1])
-
-wifi_index_list.sort()
-# print(wifi_index_list)
-min_distance = 10000000000
-for i in range(len(wifi_index_list)-1):
-    if min(wifi_index_list[i+1]-wifi_index_list[i],min_distance) < min_distance:
-        min_distance = min(wifi_index_list[i+1]-wifi_index_list[i],min_distance)
-
-print(min_distance)
+    global ans
+    while start <= end:
     
+        mid = (start + end) //2
+        first_wifi = arr[0]
+        count = 1    
+        for i in range(1,len(arr)):
+            if arr[i] - first_wifi >= mid:
+                count += 1
+                first_wifi = arr[i]
         
+        if count >= n:
+            start = mid + 1
+            ans = mid
+        else:
+            end = mid -1
+    
+wifi_search(house_list,1,house_list[-1]-house_list[0],num_wifi)
+print(ans)        
         
